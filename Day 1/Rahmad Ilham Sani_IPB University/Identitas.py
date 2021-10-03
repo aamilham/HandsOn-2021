83696d2943dd2b28376129294f971250a286d446
aamilham <91812284+aamilham@users.noreply.github.com>
2021-10-03 10:35:24 +0700

    Delete Day 1/Rahmad Ilham Sani_IPB University directory

--- Day 1/Rahmad Ilham Sani_IPB University/Identitas.py
+++
@@ -1,3 +0,0 @@
-print('Nama : Rahmad Ilham Sani')
-print('Jurusan : Ilmu Komputer')
-print('Alasan Ingin Masuk GDSC : Saya ingin menambah ilmu mengenai IT dan tentunya menambah wawasan serta pengalaman')
--- Day 1/Rahmad Ilham Sani_IPB University/identitas.md
+++
@@ -1,1 +0,0 @@
-![Identitas](https://user-images.githubusercontent.com/91812284/135737813-56593bac-b9e1-4fef-a9c5-1e06e42ac442.jpg)
